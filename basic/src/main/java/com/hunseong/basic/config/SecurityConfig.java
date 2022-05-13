15c4ecf9670323f9b9a64c9ec0fa8e418d4b5aec
Hunseong <phunseong@gmail.com>
2022-05-13 23:59:57 +0900

    SecurityConfig - url setting

---
+++ basic/src/main/java/com/hunseong/basic/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.hunseong.basic.config;
+
+import com.hunseong.basic.domain.Role;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Hunseong on 2022/05/13
+ */
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**")
+                .authenticated() // 인증 완료 시 접근 가능
+                .antMatchers("/manager/**")
+                .hasAnyRole(Role.ROLE_MANAGER.name(), Role.ROLE_ADMIN.name()) // 해당하는 Role만 접근 가능
+                .antMatchers("/admin/**")
+                .hasRole(Role.ROLE_ADMIN.name())
+                .anyRequest()
+                .permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm") // 로그인 페이지 url
+                .loginProcessingUrl("/login") // Security가 login process를 실행할 url
+                .defaultSuccessUrl("/"); // 로그인 성공 시 redirect 될 기본 url
+    }
+}
