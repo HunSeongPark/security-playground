f3f632a4b95fc86b240724d713c5db079f68e46c
Hunseong <phunseong@gmail.com>
2022-05-13 23:50:21 +0900

    add User domain

---
+++ basic/src/main/java/com/hunseong/basic/domain/Role.java
@@ -0,0 +1,8 @@
+package com.hunseong.basic.domain;
+
+/**
+ * Created by Hunseong on 2022/05/13
+ */
+public enum Role {
+    ROLE_USER, ROLE_MANAGER, ROLE_ADMIN
+}
---
+++ basic/src/main/java/com/hunseong/basic/domain/User.java
@@ -0,0 +1,43 @@
+package com.hunseong.basic.domain;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Created by Hunseong on 2022/05/13
+ */
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public User(String username, String password, String email, Role role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
