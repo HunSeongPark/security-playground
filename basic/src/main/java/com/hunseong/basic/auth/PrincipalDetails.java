6a34ca099199c1ce18096144b715979abed23aed
Hunseong <phunseong@gmail.com>
2022-05-14 00:23:15 +0900

    add PrincipalDetails - implements UserDetails

---
+++ basic/src/main/java/com/hunseong/basic/auth/PrincipalDetails.java
@@ -0,0 +1,60 @@
+package com.hunseong.basic.auth;
+
+import com.hunseong.basic.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by Hunseong on 2022/05/14
+ */
+public class PrincipalDetails implements UserDetails {
+
+    private final User user;
+
+    // 일반 로그인
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(() -> user.getRole().getValue());
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- basic/src/main/java/com/hunseong/basic/domain/Role.java
+++ basic/src/main/java/com/hunseong/basic/domain/Role.java
@@ -1,10 +1,12 @@
 package com.hunseong.basic.domain;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
  * Created by Hunseong on 2022/05/13
  */
+@Getter
 @RequiredArgsConstructor
 public enum Role {
     USER("ROLE_USER"), MANAGER("ROLE_MANAGER"), ADMIN("ROLE_ADMIN");
